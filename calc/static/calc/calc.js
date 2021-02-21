daf7fdda9c66dc64d94a82ff74a7971d5df7a0e8
Justin Gold <Justin.W.Gold@gmail.com>
2021-02-21 15:20:18 -0600

    Adjusted the calc.js file to be more succinct, and added keyboard entry

--- calc/static/calc/calc.js
+++ calc/static/calc/calc.js
@@ -1,60 +1,43 @@
-var a = '';
-var b = '';
-var num = [];
-var ans;
+let displayValue = '';  // The display value
+let num = [];  // An array of all digits entered
+let ans;  // Result of the calculation
 
 // All the numbers and operators input will be stored in an array "num" using function "sendNum()"
 function sendNum(digit){
-
 	num.push(digit);
-
-	if(num.length != 1){
-		a = '';
-		document.getElementById('screen').innerHTML = a;		// clearing the screen.
-	}
-
-
-	for(i=0; i<num.length ; i++){
-
-		a = a + num[i];				// concatenate the elements of the array "num" into a single string, which will be displayed on the screen
-
-	}
-
-document.getElementById('screen').innerHTML = a;	// displaying the concatenated string
-
+	displayValue = displayValue + num[num.length-1];	// concatenate the elements of the array "num" into a single string, which will be displayed on the screen
+	document.getElementById('screen').innerHTML = displayValue;	// displaying the concatenated string
 }
 
 // when the user presses "=", function "equalTo()" is called
 function equalTo(){
 	document.getElementById('screen').innerHTML = '';
-
-	for(i=0; i<num.length ; i++){
-
-		b += num[i];						// concatenating the array "num" into a single string
-	}
-
-	ans = eval(b);
-
+	ans = eval(displayValue); //eval has security concerns; but I don't think the user can inject malicious code in given the entry constraints.
 	document.getElementById('screen').innerHTML = ans;		// result display
-
-	while(num.length > 0){
-    	num.pop();				// emptying the array "num"
-	}
-
+  num = []
 	num.push(ans.toString());
-
-
+	displayValue = ans;
 }
-
 
 // When user presses "AC", function "clearScr()" is called
 function clearScr(){
 	document.getElementById('screen').innerHTML = '';
-
-	while(num.length > 0){
-    	num.pop();				// emptying the array "num"
-	}
-
-	a ='';
-	b ='';
+  num = [];
+	displayValue ='';
 }
+
+// This binds the relevant keyboard keys to the functions above; whose got time to click?
+const calcEntries = ['1','2','3','4','5','6','7','8','9','0','+', '-', '*', '/']
+
+document.addEventListener('keydown', function(event){
+	if (calcEntries.includes(event.key)) {
+		sendNum(event.key);
+	} else if (event.key === 'x') {
+		sendNum('*')
+	}	else if (event.key === '=' || event.key === 'Enter') {
+		equalTo();
+	} else if (event.key === 'Escape') {
+		clearScr();
+	}
+	else {}
+} );

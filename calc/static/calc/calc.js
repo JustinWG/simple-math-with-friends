ea3640002e9f09803ee0872abaff75c29cde5549
Justin Gold <Justin.W.Gold@gmail.com>
2021-02-22 23:37:46 -0600

    Final update complete

--- calc/static/calc/calc.js
+++ calc/static/calc/calc.js
@@ -1,58 +1,54 @@
 let displayValue = '';  // The display value
-let num = [];  // An array of all digits entered
-let ans;  // Result of the calculation
-const calcEntries = ['1','2','3','4','5','6','7','8','9','0','+', '-', '*', '/']
-let current_data = []
+let numberStack = [];  // An array of all digits entered
+const calcEntries = ['1','2','3','4','5','6','7','8','9','0','+', '-', '*', '/'];
 
 // All the numbers and operators input will be stored in an array "num" using function "sendNum()"
 function sendNum(digit){
-	num.push(digit);
-	displayValue = displayValue + num[num.length-1];	// concatenate the elements of the array "num" into a single string, which will be displayed on the screen
-	document.querySelector('.screen').innerHTML = displayValue;	// displaying the concatenated string
+  numberStack.push(digit);
+  displayValue = displayValue + numberStack[numberStack.length-1];	// concatenate the elements of the array "num" into a single string, which will be displayed on the screen
+  document.querySelector('.screen').innerHTML = displayValue;	// displaying the concatenated string
 }
 
 // when the user presses "=", function "equalTo()" is called
 function equalTo(){
-	document.querySelector('.screen').innerHTML = '';
-	ans = math.evaluate(displayValue); //math.eval is a JavaScript library from MathJS that aleviates some security concerns from standard eval;
-	ans = ans.toFixed(3);
-	document.querySelector('.screen').innerHTML = ans;	// result display
-	if (ans=='Infinity' || isNaN(ans)) {
-	} else {
-	// send JSON here
-	//This updates the server with equation
-	updateServer(displayValue, ans)
-
-	//waits 300ms to give the server time, then requests a new results list and updates the notebook.
-	setTimeout(function() {
-		getResultsData().then(data => writeNotebook(data));
-	}, 300);
-	}
-
-	num = []
-	num.push(ans.toString());
-	displayValue = ans;
+  document.querySelector('.screen').innerHTML = '';
+  let answer = math.evaluate(displayValue); //math.eval is a JavaScript library from MathJS that aleviates some security concerns from standard eval;
+  answer = answer.toFixed(3);
+  document.querySelector('.screen').innerHTML = answer;	// result display
+  if (answer=='Infinity' || isNaN(answer)) {
+  } else {
+    //This updates the server with equation
+    updateServer(displayValue, answer);
+    //waits 300ms to give the server time, then requests a new results list and updates the notebook.
+    setTimeout(function() {
+      getResultsData().then(data => writeNotebook(data));
+    }, 300);
+  }
+  //Resets the screen and numbers for next calculation
+  numberStack = []
+  // numberStack.push(answer.toString());
+  displayValue = [];
 }
 
 // When user presses "AC", function "clearScr()" is called
 function clearScr(){
-	document.querySelector('.screen').innerHTML = '';
-  num = [];
-	displayValue ='';
+  document.querySelector('.screen').innerHTML = '';
+  numberStack = [];
+  displayValue ='';
 }
 
 async function updateServer(expression, result) {
-		const data = {'expression': expression, 'result': result};
+    const data = {'expression': expression, 'result': result};
 
-		const response = await fetch('/api/new', {
-	  method: 'POST',
-	  headers: {
-	    'Content-Type': 'application/json',
-	  },
-	  body: JSON.stringify(data),
-		});
-		const post = await response.json();
-		return post;
+    const response = await fetch('/api/new', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+    });
+    const post = await response.json();
+    return post;
 }
 
 async function getResultsData() {
@@ -62,39 +58,34 @@ async function getResultsData() {
 }
 
 function writeNotebook(data) {
-	if (current_data === data) {
-		{}
-	} else {
-		notebook = document.querySelector('.paperContent');
-		notebook.innerHTML = "Results"
-		data.forEach(function(entry) {
-			listElement = document.createElement("li");
-			lineText = `${entry.expression} = ${entry.result}`;
-			listElement.innerHTML = lineText;
-			notebook.appendChild(listElement);
-		});
-	}
-	current_data = data
+    notebook = document.querySelector('.paperContent');
+    notebook.innerHTML = "Results";
+    data.forEach(function(entry) {
+      listElement = document.createElement("li");
+      lineText = `${entry.expression} = ${entry.result}`;
+      listElement.innerHTML = lineText;
+      notebook.appendChild(listElement);
+    });
 }
 
 function periodicUpdate() {
-	getResultsData().then(data => writeNotebook(data));
-	setTimeout(periodicUpdate, 3000);
+  getResultsData().then(data => writeNotebook(data));
+  setTimeout(periodicUpdate, 3000);
 }
 
 periodicUpdate();
 
 // This binds the relevant keyboard keys to the functions above; whose got time to click?
 document.addEventListener('keydown', function(event){
-	if (calcEntries.includes(event.key)) {
-		sendNum(event.key);
-	} else if (event.key === 'x') {
-		sendNum('*')
-	}	else if (event.key === '=' || event.key === 'Enter') {
-		equalTo();
-	} else if (event.key === 'Escape') {
-		clearScr();
-	} else if (event.key === 'Backspace') {
-		clearScr();
-	} else {}
-} );
+  if (calcEntries.includes(event.key)) {
+    sendNum(event.key);
+  } else if (event.key === 'x') {
+    sendNum('*');
+  }	else if (event.key === '=' || event.key === 'Enter') {
+    equalTo();
+  } else if (event.key === 'Escape') {
+    clearScr();
+  } else if (event.key === 'Backspace') {
+    clearScr();
+  } else {}
+});
